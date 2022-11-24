25ac41507d3c5ac250a58630e93badfb0029ff98
MAMG1DAM <migmargim@alu.edu.gva.es>
2022-11-24 10:46:19 +0100

    Codigo Añadido

--- Ejercicio control de versiones tema 3/Ejercicio control de versiones tema 3/Form1.Designer.cs
+++ Ejercicio control de versiones tema 3/Ejercicio control de versiones tema 3/Form1.Designer.cs
@@ -30,10 +30,10 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.cbUrgente = new System.Windows.Forms.CheckBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txtTelegrama = new System.Windows.Forms.TextBox();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
@@ -44,6 +44,7 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "Calcular";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
@@ -54,15 +55,15 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "Texto";
             // 
-            // checkBox1
+            // cbUrgente
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(151, 300);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(76, 17);
-            this.checkBox1.TabIndex = 2;
-            this.checkBox1.Text = "¿Urgente?";
-            this.checkBox1.UseVisualStyleBackColor = true;
+            this.cbUrgente.AutoSize = true;
+            this.cbUrgente.Location = new System.Drawing.Point(151, 300);
+            this.cbUrgente.Name = "cbUrgente";
+            this.cbUrgente.Size = new System.Drawing.Size(76, 17);
+            this.cbUrgente.TabIndex = 2;
+            this.cbUrgente.Text = "¿Urgente?";
+            this.cbUrgente.UseVisualStyleBackColor = true;
             // 
             // label2
             // 
@@ -73,30 +74,30 @@
             this.label2.TabIndex = 3;
             this.label2.Text = "Coste:";
             // 
-            // textBox1
+            // txtTelegrama
             // 
-            this.textBox1.Location = new System.Drawing.Point(151, 101);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(506, 184);
-            this.textBox1.TabIndex = 4;
+            this.txtTelegrama.Location = new System.Drawing.Point(151, 101);
+            this.txtTelegrama.Multiline = true;
+            this.txtTelegrama.Name = "txtTelegrama";
+            this.txtTelegrama.Size = new System.Drawing.Size(506, 184);
+            this.txtTelegrama.TabIndex = 4;
             // 
-            // textBox2
+            // txtPrecio
             // 
-            this.textBox2.Location = new System.Drawing.Point(191, 334);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(184, 20);
-            this.textBox2.TabIndex = 5;
+            this.txtPrecio.Location = new System.Drawing.Point(191, 334);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(184, 20);
+            this.txtPrecio.TabIndex = 5;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.txtTelegrama);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.cbUrgente);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -110,10 +111,10 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox cbUrgente;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txtTelegrama;
+        private System.Windows.Forms.TextBox txtPrecio;
     }
 }
 
